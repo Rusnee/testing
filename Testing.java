d7d638e9cbf118692206359795d04d5014a62104
Rusnee <kat.rusnee.buena@gmail.com>
2015-02-18 14:30:44 +0700

    Create Testing.java

---
+++ Testing.java
@@ -0,0 +1,10 @@
+class Oop
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("OOP");
+		System.out.println("Rusnee Buena");
+		System.out.println("ID-5410110415");
+		System.out.println("Sec03");
+	}
+}
